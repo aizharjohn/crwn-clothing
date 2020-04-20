c3922b82c0504dcc63fda675745b572347414ca4
aizhar <aizhar.quindoza@gmail.com>
2020-04-20 14:01:45 +0400

    Started adding redux into the app

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
